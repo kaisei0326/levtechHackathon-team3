97d30d4f0425213c88c116711b9f73294b3cbf38
ayagifc <79235899+ayagifc@users.noreply.github.com>
2022-12-10 15:25:49 +0900

    getとpostの細かい修正

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,15 +4,8 @@ import { useMutateLogin } from '../api/useMutateLogin';
 import { useQueryLogin } from '../api/useQueryLogin';
 
 export const SignIn = () => {
-  // const { data, status } = useQueryLogin();
-  // console.log(data);
+  const { data, status } = useQueryLogin();
   const { useLogin } = useMutateLogin();
-  const formData = {
-    csrftoken: '3feb5319afbd26e9cc831e43f4da839718a565ca8f0a4fe5889835347c2051ae',
-    name: 'aaa',
-    password: 'password'
-  }
-  // useLogin.mutate(formData);
 
   return (
     <>
