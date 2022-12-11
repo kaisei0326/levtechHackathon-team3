a691373a4942e874d81ba2da76e9f2ca97ea869d
kaisei0326 <kaisei0326@gmail.com>
2022-12-11 07:29:25 +0000

    最終調整

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,9 @@ import { SignOut } from './SignOut';
 export const Home = () => {
   return (
     <>
-      <Link to={`/signin`}>サインイン</Link>
-      <Link to={`/signup`}>サインアップ</Link>
-      <Link to={`/albams`}>アルバム(仮置き)</Link>
-      <SignOut />
+      <Link to={`/signin`}>サインイン </Link>
+      <Link to={`/signup`}> サインアップ </Link>
+      <Link to={`/groups`}> 参加グループ </Link>
     </>
   );
 }
