6e084967a6ac59f15ea7ceaf462f570ec8d8525a
ayagifc <79235899+ayagifc@users.noreply.github.com>
2022-12-11 11:39:06 +0900

    作業途中

--- src/components/Albams.js
+++ src/components/Albams.js
@@ -9,6 +9,8 @@ import {
     SimpleGrid,
     Image,
   } from '@mantine/core';
+import { useSearchParams } from 'react-router-dom';
+import { getData } from '../api/getData';
 
   const mockdata = [
     {
@@ -77,47 +79,84 @@ import {
     },
 	}));
 
-export function Albams() {
-        const { classes, theme } = useStyles();
-        const features = mockdata.map((feature) => (
-          <Card key={feature.title} shadow="xl" radius="md" className={classes.card} p="xl" component="a" href={'./photos?t='+feature.title}>
-            <Card.Section>
-							<Image
-                src = {feature.photo}
-                height={160}
-                alt="Norway"
-            	/>
-						</Card.Section>
+
+
+export async function Albams() {
+  const { classes, theme } = useStyles();
+  const guniq = '1';
+
+  // URLからguniqを取得する
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // const guniq = searchParams.get(guniq);
+
+  const url = 'https://tadanodomain.gq:9090/v1/albams?guniq=' + guniq;
+  const { data } = await getData(url);
+  const albamList = data.map((albam) => {
+    <Card key={albam.title} shadow="xl" radius="md" className={classes.card} p="xl" component="a" href={'./photos?t='+albam.name}>
+      <Card.Section>
+        <Image
+          src = {albam.photo}
+          height={160}
+          alt="Norway"
+        />
+      </Card.Section>
+      <Text size="lg" weight={500} className={classes.cardTitle} mt="md">
+        {albam.name}
+      </Text>
+      {/* <Text size="sm" color="dimmed" mt="sm">
+        {albam.description}
+      </Text> */}
+    </Card>
+  })
+
+  console.log(data);
+  // dataをmockdataと入れ替える
+
+  const newAlbam = () => {
+
+  }
+
+  const features = mockdata.map((feature) => (
+    <Card key={feature.title} shadow="xl" radius="md" className={classes.card} p="xl" component="a" href={'./photos?t='+feature.title}>
+      <Card.Section>
+        <Image
+          src = {feature.photo}
+          height={160}
+          alt="Norway"
+        />
+      </Card.Section>
 {/*           <feature.icon size={50} stroke={2} color={theme.fn.primaryColor()} /> */}
-            <Text size="lg" weight={500} className={classes.cardTitle} mt="md">
-              {feature.title}
-            </Text>
-            <Text size="sm" color="dimmed" mt="sm">
-              {feature.description}
-            </Text>
-            
-          </Card>
-        ));
-        return (
-          <Container size="lg" py="xl">
-            <Group position="right">
-              <Badge variant="filled" size="lg">
-                サインアウト
-              </Badge>
-            </Group>
+      <Text size="lg" weight={500} className={classes.cardTitle} mt="md">
+        {feature.title}
+      </Text>
+      <Text size="sm" color="dimmed" mt="sm">
+        {feature.description}
+      </Text>
       
-            <Title order={2} className={classes.title} align="center" mt="sm">
-              首里高 3年
-            </Title>
-      
+    </Card>
+  ));
+
+
+  return (
+    <Container size="lg" py="xl">
+      <Group position="right">
+        <Badge variant="filled" size="lg">
+          サインアウト
+        </Badge>
+      </Group>
+
+      <Title order={2} className={classes.title} align="center" mt="sm">
+        首里高 3年
+      </Title>
+
 {/*          <Text color="dimmed" className={classes.description} align="center" mt="md">
-              Every once in a while, you’ll see a Golbat that’s missing some fangs. This happens when
-              hunger drives it to try biting a Steel-type Pokémon.
-            </Text>
+        Every once in a while, you’ll see a Golbat that’s missing some fangs. This happens when
+        hunger drives it to try biting a Steel-type Pokémon.
+      </Text>
 */}   
-            <SimpleGrid cols={3} spacing="xl" mt={50} breakpoints={[{ maxWidth: 'md', cols: 1 }]}>
-              {features}
-            </SimpleGrid>
-          </Container>
-        );
+      <SimpleGrid cols={3} spacing="xl" mt={50} breakpoints={[{ maxWidth: 'md', cols: 1 }]}>
+        {features}
+      </SimpleGrid>
+    </Container>
+  );
 }
