16c8795f7915cbf2163a3afb5a2f73e517e9201c
ayagifc <79235899+ayagifc@users.noreply.github.com>
2022-12-11 10:48:09 +0900

    cookie登録と削除

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,10 +1,13 @@
 import { Button } from "@mantine/core";
+import { useCookies } from "react-cookie";
 import { getData } from "../api/getData";
 
 export const SignOut = () => {
-  const url = 'https://tadanodomain.gq:9090/v1/logout'
+  const url = 'https://tadanodomain.gq:9090/v1/logout';
+  const [cookies, setCookie, removeCookie] = useCookies();
   const exec = () =>{
     getData(url);
+    removeCookie(['PHPSESSID'])
   };
 
   return (
